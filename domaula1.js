85ab95137f6c6ce3ff69a15285b752a2836b54f5
Otavin23 <alvesotavio132@gmail.com>
2021-10-22 21:44:11 -0200

    a
    a

---
+++ domaula1.js
@@ -0,0 +1,10 @@
+/// getElementoById()
+
+let textoH1 = document.getElementById("text")
+let paragrafo = document.getElementById("paragrafo");
+//let otavin = document.getElementsByClassName("")
+let text = document.querySelector("input")
+text.setAttribute("id", "atribu")
+console.log(text.getAttribute("id"))
+
+console.log(text.removeAttribute("id"))
---
+++ eventos.js
@@ -0,0 +1,3 @@
+function clicando(){
+    alert("Clicou")
+}
